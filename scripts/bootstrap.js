83ce3ae668f05d09900f24b318114947ff0f5c8a
Cheng <zcbenz@gmail.com>
2024-04-29 08:02:06 +0900

    Update gn settings for Node 22

--- scripts/bootstrap.js
+++ scripts/bootstrap.js
@@ -41,6 +41,7 @@ const commonConfig = [
   'v8_promise_internal_field_count=1',
   'v8_scriptormodule_legacy_lifetime=true',
   'v8_enable_maglev=false',
+  'v8_enable_fuzztest=false',
   // TODO(zcbenz): Node.js manually updates this value in common.gypi, we should
   // find out a better procedure without manual updating source code.
   'v8_embedder_string="-node.0"',
